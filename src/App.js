3b6c142ff8bbcd022843f7c3abf012e8f47f6eaf
RamandeepVandal <ramandeepvandal@gmail.com>
2022-09-12 18:55:30 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  // city
+  const [city, setCity] = useState("Surrey");
+  // temperature
+  let [temperature, setTemperature] = useState(0);
+  // weather
+  let [weather, setWeather] = useState("");
+  // weather icon
+  let [icon, setIcon] = useState("");
+
+  // get data from api
+  const getForecast = () => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_WEATHER_API_KEY }&units=metric`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setTemperature(temperature = data.main.temp);
+        setWeather(weather = data.weather[0].description);
+        setIcon(icon = data.weather[0].icon);
+      });
+  }
+
+  // handle the user input
+  const cityChange = (e) => {
+    setCity(e.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>{city==="Surrey" ? getForecast() : null}</>
+      <div className="search">
+        <input type="text" id="usr-search" placeholder='Enter a City...' value={city} onChange={cityChange} />
+        <button id="search-btn" onClick={getForecast}>Search</button>
+      </div>
+      <div className="current-weather">
+        <div className="weather-info">
+          <img src={` http://openweathermap.org/img/wn/${icon}@2x.png`} />
+          <p>Today</p>
+          <h1>{city}</h1>
+          <p>Temperature: {Math.floor(temperature)}°</p>
+          <p>{weather}</p>
+        </div>
+      </div>
     </div>
   );
 }
